acb4a27a0ac25f1a89f1d860b64e0d27f5d58b51
Masagus Hariadi Arief <adi.arief77@gmail.com>
2021-02-18 21:51:36 +0700

    update: responsive layout

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -5,7 +5,9 @@ import { Layout } from 'Components'
 export default function Home() {
 	return (
 		<Layout>
-			<h1>ini home</h1>
+			<div className='home'>
+				<h1>ini home</h1>
+			</div>
 		</Layout>
 	)
 }
