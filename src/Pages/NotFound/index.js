df7eb4caba39f5b5a2f8bbc5f594973fc8f11f8f
Masagus Hariadi Arief <adi.arief77@gmail.com>
2021-02-18 21:16:02 +0700

    formater: page 404

--- src/Pages/NotFound/index.js
+++ src/Pages/NotFound/index.js
@@ -1,20 +1,16 @@
 export default function NotFound({ history }) {
-  return (
-    <div className="not-found container">
-      <div className="not-found__container">
-        <div className="not-found__desc">
-          <span>404</span>
-          <span>UH OH! You're lost.</span>
-          <span>
-            The page you are looking for does not exist. How you got here is a
-            mystery. But you can click the button below to go back to the
-            homepage.
-          </span>
-        </div>
-        <button className="not-found__button" onClick={() => history.push("/")}>
-          Go to Home
-        </button>
-      </div>
-    </div>
-  );
+	return (
+		<div className='not-found container'>
+			<div className='not-found__container'>
+				<div className='not-found__desc'>
+					<span>404</span>
+					<span>UH OH! You're lost.</span>
+					<span>The page you are looking for does not exist. How you got here is a mystery. But you can click the button below to go back to the homepage.</span>
+				</div>
+				<button className='not-found__button' onClick={() => history.push('/')}>
+					Go to Home
+				</button>
+			</div>
+		</div>
+	)
 }
